e4f7dfb698439ba6f96ae2016f4a43c2852ad0bb
Nicolas Berbiche <nicolas@normie.dev>
2021-02-08 19:38:54 -0500

    Migrate state to B2 bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,16 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    bucket = "normie-dev-terraform"
+    key = "normie-dev/terraform.tfstate"
+    # Literally doesn't matter with Backblaze B2
+    region = "us-west-1"
+    # Region doesn't matter with B2
+    skip_region_validation = true
+    # Do not use the STS API (S3-only)
+    skip_credentials_validation = true
+  }
 
   required_providers {
     ovh = {
