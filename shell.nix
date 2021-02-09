61945dbadd8b8ca719cced566c6c9686eca367df
Nicolas Berbiche <nicolas@normie.dev>
2021-02-08 19:37:02 -0500

    Simplify flake-compat stuff

--- shell.nix
+++ shell.nix
@@ -1,8 +1,5 @@
 let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  src = fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-    sha256 = lock.nodes.flake-compat.locked.narHash;
-  };
+  lock = builtins.getFlake (toString ./.);
+  src = lock.inputs.flake-compat.outPath;
 in
 (import src { src = ./.; }).defaultNix
